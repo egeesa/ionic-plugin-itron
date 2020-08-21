0fcaea143125cb5784a917131dc2389d477f2abe
EGEE\eca <emilie.carreras@egee.fr>
2020-08-21 16:56:35 +0200

    ligne ajoutait lors du plugin add automatiquement

--- www/ItronBridge.js
+++ www/ItronBridge.js
@@ -1,34 +1,29 @@
-cordova.define("cordova-plugin-itronbridge.ItronBridge", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    
-        module.exports.openConnection = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'openConnection', [arg0]);
-        };
-    
-        module.exports.closeConnection = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'closeConnection', [arg0]);
-        };
-    
-        module.exports.readCyble = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'readCyble', [arg0]);
-        };
-    
-        module.exports.readCyblePolling = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'readCyblePolling', [arg0]);
-        };
+var exec = require('cordova/exec');
 
-        module.exports.readPulsePolling = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'readPulsePolling', [arg0]);
-        };
-    
-        module.exports.readPulse = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'readPulse', [arg0]);
-        };
-    
-        module.exports.updateLicense = function (arg0, success, error) {
-            exec(success, error, 'ItronBridge', 'updateLicense', [arg0]);
-        };
-    
-    
-    });
-    
+module.exports.openConnection = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'openConnection', [arg0]);
+};
+
+module.exports.closeConnection = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'closeConnection', [arg0]);
+};
+
+module.exports.readCyble = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'readCyble', [arg0]);
+};
+
+module.exports.readCyblePolling = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'readCyblePolling', [arg0]);
+};
+
+module.exports.readPulsePolling = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'readPulsePolling', [arg0]);
+};
+
+module.exports.readPulse = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'readPulse', [arg0]);
+};
+
+module.exports.updateLicense = function (arg0, success, error) {
+	exec(success, error, 'ItronBridge', 'updateLicense', [arg0]);
+};
